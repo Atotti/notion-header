96bf481ddc93a298577334ced9934723f4850969
Ayuto Tsutsumi <56724676+Atotti@users.noreply.github.com>
2024-10-23 22:49:02 +0900

    initial deploy

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
 import { Hono } from 'hono';
-import { getCurrentFormattedDate } from './utils/dateUtils';
+import { getCurrentFormattedDate, getCurrentFormattedTime } from './utils/dateUtils';
+import { partlyCloudy } from './utils/icon';
 
 const app = new Hono();
 
 app.get('/svg', async (c) => {
-  const now = getCurrentFormattedDate();
+  const date = getCurrentFormattedDate();
+  const time = getCurrentFormattedTime();
 
   const svgContent = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="280">
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="200">
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Rampart+One&amp;family=Zen+Kaku+Gothic+New&amp;display=swap');
+      text {
+        font-family: 'Zen kaku Gothic New', sans-serif;
+      }
+    </style>
       <rect width="100%" height="100%" fill="lightgray" />
-      <text x="50%" y="40%" dominant-baseline="middle" text-anchor="middle" font-size="24" fill="black">
-        ${now}
+      <text x="20%" y="30%" dominant-baseline="middle" text-anchor="middle" font-size="50" fill="black">
+        ${date}
+      </text>
+      <text x="20%" y="60%" dominant-baseline="middle" text-anchor="middle" font-size="60" fill="black" letter-spacing="4">
+        ${time}
+      </text>
+      <svg x="55%" y="5%" width="100" height="100">
+        ${partlyCloudy}
+      </svg>
+      <text x="60%" y="70%" dominant-baseline="middle" text-anchor="middle" font-size="30" fill="black" letter-spacing="4">
+        Nakano, Tokyo 25°C
       </text>
     </svg>
   `;
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,3 +1,11 @@
 export function getCurrentFormattedDate(): string {
-    return new Date().toLocaleString();
+    const now = new Date();
+    const options: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric', weekday: 'short' };
+    return new Intl.DateTimeFormat('ja-JP', options).format(now);
+}
+
+export function getCurrentFormattedTime(): string {
+    const now = new Date();
+    const options: Intl.DateTimeFormatOptions = { hour: 'numeric', minute: 'numeric'};
+    return new Intl.DateTimeFormat('ja-JP', options).format(now);
 }
